609d756b3936ecbe31a3445bae9cb60abee7f9e1
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-21 23:28:04 -0500

    comment out regex for address validation - not working correctly - needs a fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,8 +10,8 @@ class Restaurant < ApplicationRecord
     message: "must be in the format (987) 654-3210"}
   validates :website, format: {with: /\Ahttps?:\/\/.*\z/,
     message: "must start with http:// or https://"}
-  validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
-    message: "must be in the format 77 Summer Street, Boston, MA 02110"}
+  # validates :address, format: {with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+  #   message: "must be in the format 77 Summer Street, Boston, MA 02110"}
 
 
 end
